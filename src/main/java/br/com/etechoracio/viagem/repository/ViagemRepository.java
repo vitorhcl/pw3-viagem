818170734ccf8db9745a5995969e40b886fab3ad
VitorHenrique-ViniciusFaoro <VitorHenrique-ViniciusFaoro@test.com>
2023-09-26 10:59:26 -0300

    findByDestino

--- src/main/java/br/com/etechoracio/viagem/repository/ViagemRepository.java
+++ src/main/java/br/com/etechoracio/viagem/repository/ViagemRepository.java
@@ -3,6 +3,8 @@ package br.com.etechoracio.viagem.repository;
 import br.com.etechoracio.viagem.entity.Viagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ViagemRepository extends JpaRepository<Viagem, Long> {
+import java.util.List;
 
+public interface ViagemRepository extends JpaRepository<Viagem, Long> {
+    List<Viagem> findByDestino(String destino);
 }
